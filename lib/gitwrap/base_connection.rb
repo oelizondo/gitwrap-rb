31a22cfd77d15efad1362d679e6b8c7a6b15955a
oscarmarcelo95 <oscarmarcelo95@gmail.com>
2015-07-19 19:33:41 -0500

    adds module to base class

--- lib/gitwrap/base_connection.rb
+++ lib/gitwrap/base_connection.rb
@@ -3,7 +3,9 @@ require 'json'
 require 'open-uri'
 require 'gitwrap/data_creator'
 
-class GithubConnection
-	include DataCreator
-	BASE_URL = "https://api.github.com/"
+module Gitwrap
+	class GithubConnection
+		include DataCreator
+		BASE_URL = "https://api.github.com/"
+	end
 end
